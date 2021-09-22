a5f436896708c7df25580bfa2a6b2a35a415a0ab
Ezichi Ebere <tripleee@yahoo.co.uk>
2021-09-22 16:58:34 +0100

    Created service.

---
+++ app/Services/FireAndIceService.php
@@ -0,0 +1,55 @@
+<?php
+
+namespace App\Services;
+
+use Exception;
+use Illuminate\Support\Facades\Http;
+use Illuminate\Support\Facades\Log;
+
+class FireAndIceService
+{
+    private $response;
+
+    public function __construct()
+    {
+        $this->response = [
+            'status_code' => 200,
+            'status' => 'success',
+            'data' => []
+        ];
+    }
+    public function prepareData(string $name)
+    {
+        $data = $this->getdata($name);
+
+        return $data;
+    }
+
+    public function getdata(string $name)
+    {
+        $booksCollection = collect();
+
+        if (is_null($name) || $name == '') {
+            return $this->response;
+        }
+
+        try {
+            $response = Http::get("https://www.anapioficeandfire.com/api/books" . "?name=" . $name);
+
+            $books = $response->collect();
+            $booksCollection = collect();
+
+            foreach ($books as $book) {
+                $bookCollection = collect($book);
+                $bookCollection = $bookCollection->only(['name','isbn','authors','number_of_pages','publisher','country','released']);
+                $booksCollection->push($bookCollection);
+            }
+        } catch (Exception $e) {
+            Log::alert("Something went wrong!!! " . $e->getMessage());
+        }
+
+        $this->response['data'] = $booksCollection->toArray();
+
+        return $this->response;
+    }
+}
