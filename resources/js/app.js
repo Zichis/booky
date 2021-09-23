2e21a768d2e1fc27bbd1b0d8a42e01764d79b4d3
Ezichi Ebere <tripleee@yahoo.co.uk>
2021-09-23 14:01:20 +0100

    Updated app.js

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,1 +1,28 @@
 require('./bootstrap');
+
+import Vue from 'vue';
+window.Vue = require('vue');
+
+import App from './App.vue';
+import VueRouter from 'vue-router';
+import VueAxios from 'vue-axios';
+import axios from 'axios';
+import {routes} from './routes';
+import vSelect from 'vue-select';
+import 'vue-select/dist/vue-select.css';
+
+Vue.component('v-select', vSelect);
+
+Vue.use(VueRouter);
+Vue.use(VueAxios, axios);
+
+const router = new VueRouter({
+    mode: 'history',
+    routes: routes
+});
+
+const app = new Vue({
+    el: '#app',
+    router: router,
+    render: h => h(App),
+});
