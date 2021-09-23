7f079d787bb6398921f721ddcfcab0eedc2ed9e4
Ezichi Ebere <tripleee@yahoo.co.uk>
2021-09-23 03:32:49 +0100

    Fixed response status and codes.

--- app/Http/Controllers/Api/v1/BooksController.php
+++ app/Http/Controllers/Api/v1/BooksController.php
@@ -12,6 +12,17 @@ use Illuminate\Support\Facades\Log;
 
 class BooksController extends Controller
 {
+    protected $statusCode;
+    protected $statusText;
+    protected $message;
+
+    public function __construct()
+    {
+        $this->statusCode = 500;
+        $this->statusText = 'failed';
+        $this->message = 'Something went wrong';
+    }
+
     /**
      * Display a listing of the resource.
      *
@@ -19,11 +30,14 @@ class BooksController extends Controller
      */
     public function index()
     {
+        $this->statusText = 'success';
+        $this->statusCode = 200;
+
         return response()->json([
-            'status_code' => 201,
-            'status' => 'success',
+            'status_code' => $this->statusCode,
+            'status' => $this->statusText,
             'data' => Book::all()
-        ]);
+        ])->setStatusCode($this->statusCode, $this->statusText);
     }
 
     /**
@@ -34,17 +48,23 @@ class BooksController extends Controller
      */
     public function store(BookPostRequest $request)
     {
-        $book = Book::create(
-            $request->only([
-                'name','isbn','authors','country','number_of_pages','publisher','release_date'
-            ])
-        );
+        try {
+            $book = Book::create(
+                $request->only([
+                    'name','isbn','authors','country','number_of_pages','publisher','release_date'
+                ])
+            );
+            $this->statusCode = 201;
+            $this->statusText = 'success';
+        } catch(Exception $exception) {
+            Log::alert("Something went wrong. Book not created. " . $exception->getMessage());
+        }
 
         return response()->json([
-            'status_code' => 201,
-            'status' => 'success',
+            'status_code' => $this->statusCode,
+            'status' => $this->statusText,
             'data' => $book
-        ]);
+        ])->setStatusCode(201, 'success');
     }
 
     /**
@@ -57,13 +77,15 @@ class BooksController extends Controller
     {
         try {
             $book = Book::find($id);
+            $this->statusCode = 200;
+            $this->statusText = 'success';
         } catch (Exception $e) {
             Log::alert("Something went wrong. " . $e->getMessage());
         }
 
         return response()->json([
-            'status_code' => 201,
-            'status' => 'success',
+            'status_code' => $this->statusCode,
+            'status' => $this->statusText,
             'data' => $book == null ?[]:$book // If book is null return empty array
         ]);
     }
@@ -85,6 +107,9 @@ class BooksController extends Controller
                     'name','isbn','authors','country','number_of_pages','publisher','release_date'
                 ])
             );
+            $this->statusCode = 200;
+            $this->statusText = 'success';
+            $this->message = 'The book My First Book was updated successfully';
         } catch (Exception $exception) {
             Log::alert("Something went wrong. " . $exception->getMessage());
         } catch (Error $error) {
@@ -92,10 +117,11 @@ class BooksController extends Controller
         }
 
         return response()->json([
-            'status_code' => 201,
-            'status' => 'success',
+            'status_code' => $this->statusCode,
+            'status' => $this->statusText,
+            'message' => $this->message,
             'data' => $book == null ?[]:$book // If book is null return empty array
-        ]);
+        ])->setStatusCode($this->statusCode, $this->statusText);
     }
 
     /**
@@ -106,18 +132,25 @@ class BooksController extends Controller
      */
     public function destroy($id)
     {
+        $book = Book::find($id);
+
         try {
-            $book = Book::find($id);
             $book->delete();
-            $data = [];
+            $this->statusCode = 204;
+            $this->statusText = 'success';
+            $this->message = 'The book My First Book was updated successfully';
+            $book = null;
         } catch (Exception $e) {
             Log::alert("Something went wrong. " . $e->getMessage());
+        } catch (Error $error) {
+            Log::alert("An error occured. " . $error->getMessage());
         }
 
         return response()->json([
-            'status_code' => 204,
-            'status' => 'success',
-            'data' => empty($data) ?$data:$book // Return book data if delete failed
-        ]);
+            'status_code' => $this->statusCode,
+            'status' => $this->statusText,
+            'message' => $this->message,
+            'data' => is_null($book) ? []:$book
+        ])->setStatusCode($this->statusCode, $this->statusText);
     }
 }
